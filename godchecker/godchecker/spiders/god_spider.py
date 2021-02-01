ef4d9d16aaaecbd21ec466f1607c450fcbb3c9b7
Ivan Andika <ivanandika@u.nus.edu>
2021-02-01 23:11:27 +0800

    Resolve small bugs regarding pantheons_page_link

--- godchecker/godchecker/spiders/god_spider.py
+++ godchecker/godchecker/spiders/god_spider.py
@@ -11,11 +11,12 @@ class GodSpider(scrapy.Spider):
 
     def parse_mythology(self, response):
         pantheons_page_link = response.css('.leftbar-tile a')
-        yield response.follow(pantheons_page_link, self.parse_pantheon)
+        # error
+        yield from response.follow_all(pantheons_page_link, self.parse_pantheon)
     
     def parse_pantheon(self, response):
         god_page_links = response.css('.search-result a')
-        yield response.follow(god_page_links, self.parse_god)
+        yield from response.follow_all(god_page_links, self.parse_god)
 
     def parse_god(self, response):
         # def extract_with_css(query):
@@ -24,6 +25,7 @@ class GodSpider(scrapy.Spider):
         attributes = [ attr.strip()[:-1] for attr in response.css('div.pullout-panel p::text').getall() if len(attr.strip()) > 0]
         values = response.css('div.pullout-panel strong::text').getall()
         facts_and_figures = dict(zip(attributes, values))
+        print(facts_and_figures)
         yield facts_and_figures
 
         
